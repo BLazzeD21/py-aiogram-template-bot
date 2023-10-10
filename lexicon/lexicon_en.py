5fbb0b3f18b6662ec5394b2092441bbe97e4fa9a
Nikita <niksbi2020@gmail.com>
2023-10-10 23:59:33 +0300

    Changed lexicon

--- lexicon/lexicon_en.py
+++ lexicon/lexicon_en.py
@@ -40,10 +40,3 @@ MAIN_BUTTONS: dict[str, str] = {
     'third_inline_button_pressed': 'Button 3',
     'fourth_inline_button_pressed': 'Button 4',
 }
-
-SECOND_BUTTONS: dict[str, str] = {
-    '': 'Button 1',
-    'second_inline_button_pressed': 'Button 2',
-    'third_inline_button_pressed': 'Button 3',
-    'fourth_inline_button_pressed': 'Button 4',
-}
