f90735fff0dd69377da9c692dd0298c9eabf9e73
Alexandr Sekerin <blazzed21@gmail.com>
2023-10-17 13:40:39 +0300

    Admin filter optimized

--- bot.py
+++ bot.py
@@ -11,14 +11,14 @@ async def main() -> None:
   BOT_TOKEN: str = configuration.tg_bot.token
   
   bot: Bot = Bot(token=BOT_TOKEN, parse_mode="html")
-  dp: Dispatcher = Dispatcher()
+
+  dp: Dispatcher = Dispatcher(configuration=configuration)
 
   dp.startup.register(set_main_menu)
 
   dp.include_router(handlers.admin_handlers.router)
   dp.include_router(handlers.user_handlers.router)
   dp.include_router(handlers.other_handlers.router)
-
 
   await bot.delete_webhook(drop_pending_updates=True)
   await dp.start_polling(bot)
--- handlers/admin_handlers.py
+++ handlers/admin_handlers.py
@@ -2,16 +2,14 @@ from aiogram.types import Message
 from aiogram.filters import Command
 from aiogram import Router
 
-from utils.get_admins import get_admin_ids
 from filters.filters import IsAdmin
 from keyboards.reply_keyboards import main_kb
 from lexicon import LEXICON
 
 router = Router()
-admin_ids = get_admin_ids()
 
 # ---------------------- Command handlers ----------------------
 
-@router.message(Command(commands='admin'), IsAdmin(admin_ids))
+@router.message(Command(commands='admin'), IsAdmin())
 async def process_help_command(message: Message):
     await message.answer(text=LEXICON['admin'], reply_markup = main_kb)
