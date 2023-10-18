4ce9e13d12eb33bec2985b818537eb14938aff36
Alexandr Sekerin <blazzed21@gmail.com>
2023-10-18 17:30:36 +0300

    Added new text and commands

--- lexicon/lexocon_commands.py
+++ lexicon/lexocon_commands.py
@@ -1,5 +1,8 @@
 LEXICON_COMMANDS: dict[str, str] = {
     '/start': 'desription',
     '/help': 'desription',
+    '/registration': 'desription',
+    '/profile': 'desription',
     '/admin': 'desription',
+    '/cancel': 'desription',
 }
