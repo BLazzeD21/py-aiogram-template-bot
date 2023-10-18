da9ad971f691f705a9078b0bb3986e4f991addba
Alexandr Sekerin <blazzed21@gmail.com>
2023-10-18 17:30:16 +0300

    Added redis storage

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.redis import RedisStorage, Redis
 
 from config_data.config import Config, load_config
 from keyboards.set_menu import set_main_menu
@@ -10,14 +11,20 @@ async def main() -> None:
 
   BOT_TOKEN: str = configuration.tg_bot.token
   
+  # Инициализируем Redis
+  redis = Redis(host='localhost')
+
   bot: Bot = Bot(token=BOT_TOKEN, parse_mode="html")
 
-  dp: Dispatcher = Dispatcher(configuration=configuration)
+  storage = RedisStorage(redis=redis)
+
+  dp: Dispatcher = Dispatcher(bot=bot, configuration=configuration, storage=storage)
 
   dp.startup.register(set_main_menu)
 
   dp.include_router(handlers.admin_handlers.router)
   dp.include_router(handlers.user_handlers.router)
+  dp.include_router(handlers.form_handlers.router)
   dp.include_router(handlers.other_handlers.router)
 
   await bot.delete_webhook(drop_pending_updates=True)
